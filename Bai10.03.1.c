3dd28bd250e393a584484eae7d2a45dacdc6d4c9
q5nguyenn <q5nguyenn@gmail.com>
2022-06-15 11:20:39 +0700

    Update

--- Bai10.03.1.c
+++ Bai10.03.1.c
@@ -3,11 +3,14 @@
 
 void main (void)
 {
-	int i, n;
+	int i = 1, n;
 	printf ("\nNhap vao bang cuu chuong can in: ");
 	scanf ("%d", &n);
-	for (i = 1; i <= 9; i++)
+	while (i <= 9)
+	{
 		printf ("\t%d x %d = %d\n", n, i, n * i);
+		i++;
+	}
 }
 		
 
